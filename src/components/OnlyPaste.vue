8feaabfb129097e8eb6d1d0edc8bd670b4ea586b
moh123mos <moh162534aaa@gmail.com>
2025-01-21 22:04:39 +0200

    fix

--- src/components/OnlyPaste.vue
+++ src/components/OnlyPaste.vue
@@ -2,7 +2,7 @@
   <div
     @click="pasteText"
     @paste="pasteText"
-    class="flex-1 p-4 w-full h-screen cursor-pointer flex justify-center items-center flex-col text-primary-light dark:text-primary-dark"
+    class="flex-1 text-center p-4 w-full h-screen cursor-pointer flex justify-center items-center flex-col text-primary-light dark:text-primary-dark"
   >
     <h1 class="text-4xl md:text-6xl lg:text-8xl pt-10 pb-5 px-5 md:p-0">Just Paste it.</h1>
     <p class="text-sm">You can just CLICK on this section too.</p>
